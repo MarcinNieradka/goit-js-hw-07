98596f2a93d266974d20d99dd11776e24262ed0d
MarcinNieradka <nieradka.marcin@gmail.com>
2023-03-23 18:23:52 +0100

    upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const mainGallery = document.querySelector('.gallery');
+// const galleryContainer = [];
+
+galleryItems.forEach(item => {
+  const myImage = document.createElement('li');
+  myImage.innerHTML = `<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+</a>`;
+  //   galleryContainer.push(myImage);
+  mainGallery.append(myImage);
+});
+
+// mainGallery.append(...galleryContainer);
+
+// mainGallery.addEventListener('click', e => {
+//   e.preventDefault();
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+// });
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
